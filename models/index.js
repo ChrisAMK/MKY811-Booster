b98b8388056df19e78cea9893ca9b1e1b254137b
Chris <chriskl@live.com>
2020-11-27 10:13:22 +0800

    deploy

--- models/index.js
+++ models/index.js
@@ -17,7 +17,7 @@ if (config.use_env_variable) {
   });
 } else {
   console.log("Bottom 3333333333333333")
-  console.log(config.database, config, username + "2222222222")
+  console.log(config.database + config.username + "2222222222")
   var sequelize = new Sequelize(config.database, config.username, config.password, {
     host: "127.0.0.1",
     dialect: "mysql",
