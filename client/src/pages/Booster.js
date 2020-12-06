b61f35592dd750bea619b51e6525b7687b969115
Chris <chriskl@live.com>
2020-12-06 20:11:12 +0800

    added graph

--- client/src/pages/Booster.js
+++ client/src/pages/Booster.js
@@ -1,4 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, PureComponent } from "react";
+import {
+  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
 import Button from '@material-ui/core/Button';
 //import Switch from '@material-ui/core/Switch';
 
@@ -42,6 +45,30 @@ function Booster() {
             live = null;
         }
     }
+
+    const data = [
+        {
+          name: 'Monday', uv: 4000, pv: 2400, amt: 2400,
+        },
+        {
+          name: 'Tuesday', uv: 3000, pv: 1398, amt: 2210,
+        },
+        {
+          name: 'Wednesday', uv: 2000, pv: 9800, amt: 2290,
+        },
+        {
+          name: 'Thursday', uv: 2780, pv: 3908, amt: 2000,
+        },
+        {
+          name: 'Friday', uv: 1890, pv: 4800, amt: 2181,
+        },
+        {
+          name: 'Saturday', uv: 2390, pv: 3800, amt: 2500,
+        },
+        {
+          name: 'Sunday', uv: 3490, pv: 4300, amt: 2100,
+        },
+      ];
     
 
     return(
@@ -69,6 +96,49 @@ function Booster() {
                 </Button>
                 </div>
             </div>
+            <div className="row">
+                <div className="col-12 col-sm-12 col-md-12 col-lg-6 text-center">
+                    <h3>Weekly Max Temp</h3>
+                    <LineChart
+                        width={500}
+                        height={300}
+                        data={data}
+                        margin={{
+                        top: 5, right: 30, left: 20, bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                    </LineChart>
+                </div>
+                <div className="col-12 col-sm-12 col-md-12 col-lg-6 text-center">
+                    <h3>Weekly Max Temp</h3>
+                    <LineChart
+                        width={500}
+                        height={300}
+                        data={data}
+                        margin={{
+                        top: 5, right: 30, left: 20, bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                    </LineChart>
+                </div>
+            </div>
+            
+            
+            
         </React.Fragment>
     )
 }
